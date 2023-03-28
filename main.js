f42d10b4bf6e177e690b7de49520baa22a19c392
DANIEL BRACEY <24969761@edgehill.ac.uk>
2023-03-28 11:23:57 +0100

    Add JSON files to Source

--- main.js
+++ main.js
@@ -1,64 +1,39 @@
-var pageCounter = 1;
-var moduleContainer = document.getElementById('module-info');
-var btn = document.getElementById("btn");
+'use strict';
 
-btn.addEventListener("click", function(){
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://raw.githubusercontent.com/Edge-Hill-Univeristy-Web/CIS2169-Academic-Management-System/main/module-'+ pageCounter +'.json');
-  ourRequest.onload = function(){
-    //console.log(ourRequest.responseText);
-    var ourData = JSON.parse(ourRequest.responseText);
-    //console.log(ourData[0]);
-    renderHTML(ourData);
-  };
-  ourRequest.send();
-pageCounter++;
-if (pageCounter > 3){
-//btn.classList.add("hide-me");
-  btn.disabled = true;
-}
+/* global m */
+
+// Resource list
+var db = {};
+
+m.request('db').then(function (data) {
+  db = data;
 });
 
-function renderHTML(data){
-  var htmlString = "";
+m.mount(document.getElementById('resources'), {
+  view: function view() {
+    var keys = Object.keys(db);
+    var resourceList = m('ul', keys.map(function (key) {
+      return m('li', [m('a', { href: key }, '/' + key), m('sup', Array.isArray(db[key]) ? ' ' + db[key].length + 'x' : ' object')]);
+    }).concat([m('a', { href: 'db' }, '/db'), m('sup', m('em', ' state'))]));
 
-  for(i = 0; i < data.length; i++){
-    htmlString += "<p>" + data[i].Name + " is a " + data[i].Course + " has assements "; //".</p>";
-    for(ii = 0; ii < data[i].Module.Assignment.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Assignment[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Assignment[ii];
-      }
-    }
-    htmlString += ' and Learning Outcome ';
-    for(ii = 0; ii < data[i].Module.Learning_outcomes.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Learning_outcomes[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Learning_outcomes[ii];
-      }
-    }
-
-    htmlString += ' and Volume ';
-    for(ii = 0; ii < data[i].Module.Volume.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Volume[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Volume[ii];
-      }
-    }
-
-    htmlString += ' and weights ';
-    for(ii = 0; ii < data[i].Module.weights.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.weights[ii];
-      } else {
-        htmlString += " and " + data[i].Module.weights[ii];
-      }
-    }
-    htmlString += '.</p>';
+    return [m('h4', 'Resources'), keys.length ? resourceList : m('p', 'No resources found')];
   }
-  moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+});
 
-}
+// Custom routes
+var customRoutes = {};
+
+m.request('__rules').then(function (data) {
+  customRoutes = data;
+});
+
+m.mount(document.getElementById('custom-routes'), {
+  view: function view() {
+    var rules = Object.keys(customRoutes);
+    if (rules.length) {
+      return [m('h4', 'Custom routes'), m('table', rules.map(function (rule) {
+        return m('tr', [m('td', rule), m('td', '⇢ ' + customRoutes[rule])]);
+      }))];
+    }
+  }
+});
