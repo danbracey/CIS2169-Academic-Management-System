b41ec5b107874b7242d79e9d99cb9e083d2b3493
Dan Bracey <24969761@edgehill.ac.uk>
2023-04-13 08:30:11 +0100

    Add basis for Undergraduate degree programs

--- main.js
+++ main.js
@@ -1,39 +1,105 @@
-'use strict';
+var pageCounter = 1;
+var moduleContainer = document.getElementById('module-info');
+var btn = document.getElementById("btn");
 
-/* global m */
-
-// Resource list
-var db = {};
-
-m.request('db').then(function (data) {
-  db = data;
-});
-
-m.mount(document.getElementById('resources'), {
-  view: function view() {
-    var keys = Object.keys(db);
-    var resourceList = m('ul', keys.map(function (key) {
-      return m('li', [m('a', { href: key }, '/' + key), m('sup', Array.isArray(db[key]) ? ' ' + db[key].length + 'x' : ' object')]);
-    }).concat([m('a', { href: 'db' }, '/db'), m('sup', m('em', ' state'))]));
-
-    return [m('h4', 'Resources'), keys.length ? resourceList : m('p', 'No resources found')];
+/**btn.addEventListener("click", function(){
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://raw.githubusercontent.com/profharimohanpandey/CW2/master/module-'+ pageCounter +'.json');
+  ourRequest.onload = function(){
+    //console.log(ourRequest.responseText);
+    var ourData = JSON.parse(ourRequest.responseText);
+    //console.log(ourData[0]);
+    renderHTML(ourData);
+  };
+  ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3){
+//btn.classList.add("hide-me");
+    btn.disabled = true;
   }
-});
+});**/
 
-// Custom routes
-var customRoutes = {};
+function renderHTML(data){
+  var htmlString = "";
 
-m.request('__rules').then(function (data) {
-  customRoutes = data;
-});
-
-m.mount(document.getElementById('custom-routes'), {
-  view: function view() {
-    var rules = Object.keys(customRoutes);
-    if (rules.length) {
-      return [m('h4', 'Custom routes'), m('table', rules.map(function (rule) {
-        return m('tr', [m('td', rule), m('td', '⇢ ' + customRoutes[rule])]);
-      }))];
+  for(i = 0; i < data.length; i++){
+    htmlString += "<p>" + data[i].Name + " is a " + data[i].Course + " has assements "; //".</p>";
+    for(ii = 0; ii < data[i].Module.Assignment.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].Module.Assignment[ii];
+      } else {
+        htmlString += " and " + data[i].Module.Assignment[ii];
+      }
     }
+    htmlString += ' and Learning Outcome ';
+    for(ii = 0; ii < data[i].Module.Learning_outcomes.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].Module.Learning_outcomes[ii];
+      } else {
+        htmlString += " and " + data[i].Module.Learning_outcomes[ii];
+      }
+    }
+
+    htmlString += ' and Volume ';
+    for(ii = 0; ii < data[i].Module.Volume.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].Module.Volume[ii];
+      } else {
+        htmlString += " and " + data[i].Module.Volume[ii];
+      }
+    }
+
+    htmlString += ' and weights ';
+    for(ii = 0; ii < data[i].Module.weights.length; ii++){
+      if (ii == 0){
+        htmlString += data[i].Module.weights[ii];
+      } else {
+        htmlString += " and " + data[i].Module.weights[ii];
+      }
+    }
+    htmlString += '.</p>';
   }
-});
+  moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+
+}
+
+function displayDegreePrograms(toggle) {
+  switch (toggle) {
+    case 1:
+      getDegreeProgramData('undergraduate');
+      break
+    case 2:
+      getDegreeProgramData('postgraduate');
+      break
+    case 3:
+      getDegreeProgramData('research');
+  }
+}
+
+function getDegreeProgramData(word) {
+  console.log("A");
+  const ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', '/' + word  + '.json');
+  ourRequest.onload = function(){
+    //console.log(ourRequest.responseText);
+    var ourData = JSON.parse(ourRequest.responseText);
+    //console.log(ourData[0]);
+    // Find a <table> element with id="myTable":
+    var table = document.getElementById("degreePrograms");
+
+    for(let i=0; i > ourData.length; i++) {
+      var row = table.insertRow(0);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+
+      let moduleCounter = 0;
+      for(let i=0; i > ourData[i].Modules.length; i++) {
+        moduleCounter++;
+      }
+
+      cell1.innerHTML = ourData[i].Name;
+      cell2.innerHTML = moduleCounter;
+    }
+  };
+  ourRequest.send();
+}
